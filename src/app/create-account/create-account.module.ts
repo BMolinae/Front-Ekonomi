05eb0deec5f0efc6609110bf06f1696b3fd3673a
BMolinae <ben.molina@duocuc.cl>
2025-06-22 16:14:07 -0400

    Se agrego capacidad de espacio y nuevo apk

--- src/app/create-account/create-account.module.ts
+++ src/app/create-account/create-account.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CreateAccountPage } from './create-account.page';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
+    CommonModule,
     RouterModule.forChild([
       {
         path: '',
